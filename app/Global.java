71ced3b5dd78c18a2085ab0493598920894e501d
spacetoha <tohakv50@gmail.com>
2013-04-25 00:23:51 +0200

    target folder removed

--- app/Global.java
+++ app/Global.java
@@ -1,86 +1,27 @@
-import com.feth.play.module.pa.PlayAuthenticate;
-import com.feth.play.module.pa.PlayAuthenticate.Resolver;
-import com.feth.play.module.pa.exceptions.AccessDeniedException;
-import com.feth.play.module.pa.exceptions.AuthException;
-import com.google.code.morphia.Morphia;
-import com.mongodb.Mongo;
-import controllers.routes;
+import java.net.UnknownHostException;
+
 import play.Application;
 import play.GlobalSettings;
 import play.Logger;
-import play.mvc.Call;
+import play.Play;
 import service.MorphiaObject;
 
-import java.net.UnknownHostException;
+import com.google.code.morphia.Morphia;
+import com.mongodb.Mongo;
 
 public class Global extends GlobalSettings {
 
     public void onStart(final Application app) {
-        PlayAuthenticate.setResolver(new Resolver() {
-
-            @Override
-            public Call login() {
-                // Your login page
-                return routes.Account.login();
-            }
-
-            @Override
-            public Call afterAuth() {
-                // The user will be redirected to this page after authentication
-                // if no original URL was saved
-                return routes.Account.index();
-            }
-
-            @Override
-            public Call afterLogout() {
-                return routes.Account.login();
-            }
-
-            @Override
-            public Call auth(final String provider) {
-                // You can provide your own authentication implementation,
-                // however the default should be sufficient for most cases
-                return com.feth.play.module.pa.controllers.routes.Authenticate
-                        .authenticate(provider);
-            }
-
-            @Override
-            public Call onException(final AuthException e) {
-                if (e instanceof AccessDeniedException) {
-                    return routes.Application
-                            .oAuthDenied(((AccessDeniedException) e)
-                                    .getProviderKey());
-                }
-
-                // more custom problem handling here...
-
-                return super.onException(e);
-            }
-
-            @Override
-            public Call askLink() {
-                // We don't support moderated account linking in this sample.
-                // See the play-authenticate-usage project for an example
-                return null;
-            }
-
-            @Override
-            public Call askMerge() {
-                // We don't support moderated account merging in this sample.
-                // See the play-authenticate-usage project for an example
-                return null;
-            }
-        });
-
         super.beforeStart(app);
         Logger.debug("** onStart **");
         try {
-            MorphiaObject.mongo = new Mongo("127.0.0.1", 27017);
+        	Logger.debug(Play.application().configuration().getString("mongoPort"));
+            MorphiaObject.mongo = new Mongo("127.0.0.1", Play.application().configuration().getInt("mongoPort"));
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
         MorphiaObject.morphia = new Morphia();
-        MorphiaObject.datastore = MorphiaObject.morphia.createDatastore(MorphiaObject.mongo, "leaveithere");
+        MorphiaObject.datastore = MorphiaObject.morphia.createDatastore(MorphiaObject.mongo, "push2show");
         MorphiaObject.datastore.ensureIndexes();
         MorphiaObject.datastore.ensureCaps();
 
